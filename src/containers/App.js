f479eb2fbd3e9f7bd54128d1bd8346386acb7617
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-12 12:30:16 +0300

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Default WP4 config</p>
+      </div>
+    );
+  }
+}
+
+export default App;
