a9ffb0f159a02d2e727df8d1b2c84d2dad9da53b
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-13 11:16:07 +0300

    routes+adapt for CRA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./containers/App";
 import { Provider } from "react-redux";
 import store from "./store/Store";
+// if you need copy /src/ folder to Create React App - uncomment this
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -10,3 +12,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+// if you need copy /src/ folder to Create React App - uncomment this
+// registerServiceWorker();
