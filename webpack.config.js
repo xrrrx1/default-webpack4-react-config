917f20a7c34efd0b7c28b41e3807ab1ca30317ef
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-30 11:24:47 +0300

    add-sass\scss

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.s(a|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
